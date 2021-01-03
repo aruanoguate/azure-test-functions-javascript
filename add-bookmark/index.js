f85dafc3df243e2b8dc2dd28bd9676814bdb5b95
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-01-03 00:24:21 -0600

    Adding function definition

--- add-bookmark/index.js
+++ add-bookmark/index.js
@@ -1,13 +1,37 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = function (context, req) {
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    var bookmark = context.bindings.bookmark
+    if(bookmark){
+            context.res = {
+            status: 422,
+            body : "Bookmark already exists.",
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        };
+    }
+    else {
+        
+        // Create a JSON string of our bookmark.
+        var bookmarkString = JSON.stringify({ 
+            id: req.body.id,
+            url: req.body.url
+        });
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+        // Write this bookmark to our database.
+        context.bindings.newbookmark = bookmarkString;
+
+        // Push this bookmark onto our queue for further processing.
+        context.bindings.newmessage = bookmarkString;
+
+        // Tell the user all is well.
+        context.res = {
+            status: 200,
+            body : "bookmark added!",
+            headers: {
+            'Content-Type': 'application/json'
+            }
+        };
+    }
+    context.done();
+};
